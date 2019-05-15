c7eb9bf5c5b4b937af57236c1a7e6bb561ae9e17
BhushanLal <bhushanlal972@gmail.com>
2019-05-15 19:42:06 +0530

    code updated

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module ApiApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.autoload_paths << Rails.root.join('lib')
+
     # config.middleware.insert_before 0, Rack::Cors do
     #   allow do
     #     origins '*'
@@ -34,6 +34,9 @@ module ApiApp
         resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
+
+    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
